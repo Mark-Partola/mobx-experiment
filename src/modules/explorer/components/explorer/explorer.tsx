879d5ac0a606067867213f5eb24092c4ddfa7a4d
Mark Partola <mark_partola@epam.com>
2020-05-25 22:02:11 +0300

    Move files to store, implement local renaming

--- src/modules/explorer/components/explorer/explorer.tsx
+++ src/modules/explorer/components/explorer/explorer.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
+import { useObserver } from 'mobx-react';
 import styled from 'styled-components';
-import { IFileMeta } from '../../../../types/IFileMeta';
 import { File } from './components/file';
 import { useExplorerStore } from '../../store/use-explorer-store';
-
-const items: IFileMeta[] = [
-  { isDir: true, name: 'New folder', path: '/' },
-  { isDir: true, name: 'New folder (1)', path: '/' },
-  { isDir: false, name: 'New file', path: '/' },
-  { isDir: false, name: 'New file (1)', path: '/' },
-  { isDir: false, name: 'New file (2)', path: '/' },
-  { isDir: false, name: 'New file (3)', path: '/' },
-  { isDir: false, name: 'Super brand new file (4)', path: '/' },
-  { isDir: false, name: 'New file (5)', path: '/' },
-  { isDir: false, name: 'New file (6)', path: '/' },
-  { isDir: false, name: 'New file (7)', path: '/' },
-  { isDir: false, name: 'New file (8)', path: '/' },
-];
 
 const Explorer = () => {
   const explorerStore = useExplorerStore();
@@ -28,15 +14,15 @@ const Explorer = () => {
     [explorerStore]
   );
 
-  return (
+  return useObserver(() => (
     <StyledFilesLayout>
-      {items.map((item) => (
+      {explorerStore.files.map((item) => (
         <StyledFileItemLayout key={item.name}>
           <File meta={item} onRename={handleRename} />
         </StyledFileItemLayout>
       ))}
     </StyledFilesLayout>
-  );
+  ));
 };
 
 const StyledFilesLayout = styled.div`
--- src/modules/explorer/store/explorer-store.ts
+++ src/modules/explorer/store/explorer-store.ts
@@ -1,11 +1,33 @@
-import { action } from 'mobx';
+import { action, observable } from 'mobx';
+import { IFileMeta } from '../../../types/IFileMeta';
 
 export const EXPLORER_STORE_KEY = 'explorer';
 
 export class ExplorerStore {
+  @observable
+  files = [
+    { isDir: true, name: 'New folder', path: '/' },
+    { isDir: true, name: 'New folder (1)', path: '/' },
+    { isDir: false, name: 'New file', path: '/' },
+    { isDir: false, name: 'New file (1)', path: '/' },
+    { isDir: false, name: 'New file (2)', path: '/' },
+    { isDir: false, name: 'New file (3)', path: '/' },
+    { isDir: false, name: 'Super brand new file (4)', path: '/' },
+    { isDir: false, name: 'New file (5)', path: '/' },
+    { isDir: false, name: 'New file (6)', path: '/' },
+    { isDir: false, name: 'New file (7)', path: '/' },
+    { isDir: false, name: 'New file (8)', path: '/' },
+  ];
+
   @action
-  rename(params: object) {
-    // todo
-    console.log('rename', params);
+  rename(params: { meta: IFileMeta; name: string }) {
+    const file = this.files.find((file) => file.name === params.meta.name);
+
+    if (!file) {
+      // TODO: add loger service
+      return window.console.log('File not found');
+    }
+
+    file.name = params.name;
   }
 }
